c4a488e592ae5d0f20aa61ac75fa967aa6e97681
Johan Falk <johan@vaxjonexus.com>
2022-10-14 20:00:56 +0200

    Indented PowerShell lines

--- search-words/search-words.bat
+++ search-words/search-words.bat
@@ -8,16 +8,16 @@ SET File=text-document.txt
 
 REM Running commands through PowerShell, to take care of UTF-8 format
 powershell.exe -command ^
-$words="""word1""","""word2""","""word3""", """word4""", """word5""", """words with spaces""", """...""";^
-$output=,""" """;^
-foreach ($word in $words) {^
-$result = Select-String """%File%""" -Pattern \b$word\b ^;^
-if ($result) {^
-$output += """ """;^
-$output += """'""" + $word + """' was found in line """ + $result.LineNumber + """: """ + $result.Line;^
-}^
-}^
-$output;
+  $words="""word1""","""word2""","""word3""", """word4""", """word5""", """words with spaces""", """...""";^
+  $output=,""" """;^
+  foreach ($word in $words) {^
+    $result = Select-String """%File%""" -Pattern \b$word\b ^;^
+    if ($result) {^
+      $output += """ """;^
+      $output += """'""" + $word + """' was found in line """ + $result.LineNumber + """: """ + $result.Line;^
+    }^
+  }^
+  $output;
 
 REM Pausing the script to prevent the terminal window from closing
 ECHO ·
